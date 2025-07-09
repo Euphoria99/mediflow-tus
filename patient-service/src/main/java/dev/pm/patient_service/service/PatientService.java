ddfb3cb1b71f62b1d2265bdbd0695fe9e1b08b12
Euphoria99 <363pavanbhat@gmail.com>
2025-07-09 23:45:02 +0100

    patient service-1

---
+++ patient-service/src/main/java/dev/pm/patient_service/service/PatientService.java
@@ -0,0 +1,28 @@
+package dev.pm.patient_service.service;
+
+import dev.pm.patient_service.dto.PatientResponseDTO;
+import dev.pm.patient_service.mapper.PatientMapper;
+import dev.pm.patient_service.model.Patient;
+import dev.pm.patient_service.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    private PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients() {
+        List<Patient> patients = patientRepository.findAll();
+
+        List<PatientResponseDTO> patientResponseDTOs = patients.stream()
+                .map(patient -> PatientMapper.toDTO(patient)).toList();
+
+        return patientResponseDTOs;
+    }
+}
