4821587e1295425d67e26a88adec0e04726a8ba6
Euphoria99 <363pavanbhat@gmail.com>
2025-07-18 13:16:57 +0100

    update endpoint

--- patient-service/src/main/java/dev/pm/patient_service/service/PatientService.java
+++ patient-service/src/main/java/dev/pm/patient_service/service/PatientService.java
@@ -3,12 +3,15 @@ package dev.pm.patient_service.service;
 import dev.pm.patient_service.dto.PatientRequestDTO;
 import dev.pm.patient_service.dto.PatientResponseDTO;
 import dev.pm.patient_service.exception.EmailAlreadyExistsException;
+import dev.pm.patient_service.exception.PatientNotFoundException;
 import dev.pm.patient_service.mapper.PatientMapper;
 import dev.pm.patient_service.model.Patient;
 import dev.pm.patient_service.repository.PatientRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class PatientService {
@@ -36,5 +39,23 @@ public class PatientService {
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
 
         return PatientMapper.toDTO(newPatient);
+    }
+
+    public PatientResponseDTO updatePatient(UUID id, PatientRequestDTO patientRequestDTO){
+
+        Patient patient = patientRepository.findById(id).orElseThrow( ()-> new PatientNotFoundException("Patient not found with ID: "+  id));
+
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())){
+            throw new EmailAlreadyExistsException("A Patient with this email " + patientRequestDTO.getEmail() + " already exists");
+        }
+
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patient.getAddress());
+        patient.setEmail(patient.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+
+        Patient updatedatient = patientRepository.save(patient);
+
+        return PatientMapper.toDTO(updatedatient);
     }
 }
